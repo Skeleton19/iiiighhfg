8fcef420b88915d813c9b02b9d635ba26528d735
Skeleton_Gangster <50092873+Skeleton19@users.noreply.github.com>
2024-02-23 18:46:38 +0800

    Update Loader.lua

--- HitBot/Loader.lua
+++ HitBot/Loader.lua
@@ -1,5 +1,11 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Skeleton19/iiiighhfg/main/HitBot/ESP.lua"))()
 
+if game.PlaceId == 606849621 then
+loadstring(game:HttpGet("https://pastebin.com/raw/5Nkd2QYs"))()
+elseif game.PlaceId == 12355337193 then
+loadstring(game:HttpGet("https://pastebin.com/raw/K6YT8dws"))()
+elseif game.PlaceId == 13772394625 then
+loadstring(game.HttpGet("https://pastebin.com/raw/pCZ9mesW"))()
 else
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Skeleton19/iiiighhfg/main/HitBot/HitBox.lua"))()
 end
