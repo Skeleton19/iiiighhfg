b7a4b7c1991ba2d8dd558a7d749e4dbbd8373087
Skeleton_Gangster <50092873+Skeleton19@users.noreply.github.com>
2024-07-26 23:54:17 +0800

    Update FNP.lua

--- FNP.lua
+++ FNP.lua
@@ -4,26 +4,38 @@ _G.autoplay = true
 
 vim = cloneref(game:GetService("VirtualInputManager"))
 
+local offset = 17 --100 = 1 sekunda
 
-buttons = {"F","G","H","J"}
+_G.arrows = 4
+
+local buttons
 
 local module = require(game.ReplicatedStorage.Modules.Note)
 local gamemodule = require(game.ReplicatedStorage.Modules.Conductor)
 local func = module.SpawnNote
 
-function main(path,timer)
-                repeat
-                    task.wait(0.01)
-                until gamemodule.SongPos >= timer
-                local numb = path+1
-                --print("GOAT",buttons[numb],arg['Side'],arg['IsSustain'])
-                vim:SendKeyEvent(1,Enum.KeyCode[buttons[numb]],0,nil)
-            end
 
-event = Instance.new("BindableEvent",workspace)
+event = Instance.new("BindableEvent")
 
-event.Event:Connect(function(path,timer)
-    main(path,timer)
+event.Event:Connect(function(path,timer,l)
+    repeat
+        task.wait()
+    until gamemodule.SongPos >= timer-offset
+    local numb = path+1
+if _G.arrows == 4 then
+    buttons = {"F","G","H","J"}
+elseif _G.arrows == 5 then
+    buttons = {"F","G","Space","J","K"}
+elseif _G.arrows == 6 then
+    buttons = {"F","G","H","J","K","L"}
+elseif _G.arrows == 7 then
+    buttons = {"F","G","H","Space","J","K","L"}
+elseif _G.arrows == 8 then
+    buttons = {"F","G","H","J","K","L","Semicolon","Quote"}
+elseif _G.arrows == 9 then
+    buttons = {"F","G","H","J","Space","K","L","Semicolon","Quote"}
+end
+    vim:SendKeyEvent(1,Enum.KeyCode[buttons[numb]],0,nil)
 end)
 
 local o
@@ -32,8 +44,8 @@ o = hookfunction(func, function(...)
     for _,arg in pairs(args) do
         if type(arg) == "table" and arg["MustPress"] == true and arg["shouldPress"] == true then
             --print(chet,arg["NoteData"],arg['Side'],arg['IsSustain'],arg['StrumTime'],gamemodule.SongPos)
-            if _G.autoplay == true then
-                event:Fire(arg["NoteData"],arg["StrumTime"])
+             if _G.autoplay == true then
+                event:Fire(arg["NoteData"],arg["StrumTime"],arg["SustainLength"])
             end
         end
     end
